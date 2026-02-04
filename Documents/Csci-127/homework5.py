d174c452311835bfe2fa111394ea8ae3b0a32b74
Juthi Kathun <juthikathun459@gmail.com>
2026-02-04 14:25:00 -0600

    Adding hw5

---
+++ Documents/Csci-127/homework5.py
@@ -0,0 +1,24 @@
+"""
+Name : Juthi Kathun
+Email :juthi.kathun27@login.cuny.edu
+"""
+import turtle
+
+t = turtle.Turtle()
+t.speed(0)     # Set the turtle's speed to 0 (fastest)
+t.pensize(2)   # Set the turtle's pen size to 2
+t.hideturtle() # Hide the turtle
+
+
+for i in range (36): 
+    for i in range (3):
+    
+        t.color("pink")
+        t.forward(80)
+        t.right(60)
+        t.color("red")
+        t.forward (80)  
+        t.right(60)
+    t.right(10)
+
+
